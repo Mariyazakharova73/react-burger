b903900cb4034a6cd77ba88da21d150f9fe31cb3
Mariya Zakharova <zakharovamaria73@yandex.ru>
2023-03-12 00:27:42 +0400

    feat: added types for cookie

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,7 +1,7 @@
-export function setCookie(name, value, props) {
+export function setCookie(name: string, value: string, props?: any) {
   props = props || {};
   let exp = props.expires;
-  if (typeof exp == 'number' && exp) {
+  if (typeof exp == "number" && exp) {
     const d = new Date();
     d.setTime(d.getTime() + exp * 1000);
     exp = props.expires = d;
@@ -10,20 +10,20 @@ export function setCookie(name, value, props) {
     props.expires = exp.toUTCString();
   }
   value = encodeURIComponent(value);
-  let updatedCookie = name + '=' + value;
+  let updatedCookie = name + "=" + value;
   for (const propName in props) {
-    updatedCookie += '; ' + propName;
+    updatedCookie += "; " + propName;
     const propValue = props[propName];
     if (propValue !== true) {
-      updatedCookie += '=' + propValue;
+      updatedCookie += "=" + propValue;
     }
   }
   document.cookie = updatedCookie;
 }
 
-export function getCookie(name) {
+export function getCookie(name: string) {
   const matches = document.cookie.match(
-    new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)')
+    new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") + "=([^;]*)")
   );
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
